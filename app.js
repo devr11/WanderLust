469b4cb174d46f99fba52a2bb7b8a8c17c446c1d
Dev <rastogi.db1@gmail.com>
2025-12-04 12:53:20 +0530

    modified

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
+const path = require('path');
 
-app.set("view engine");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
