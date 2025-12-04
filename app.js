271a1ea0bf4581e24af48d9d5bcd7f6594839bcf
Dev <rastogi.db1@gmail.com>
2025-12-04 19:00:06 +0530

    modified

--- app.js
+++ app.js
@@ -39,7 +39,12 @@ app.get("/listings/:id", async (req, res)=>{
 })
 
 //Create Route
-
+app.post("/listings", async (req, res)=>{
+  // let {title, description, image, price, country, loaction} = req.body;
+  const newListing = new Listing(req.body.listing);
+  await newListing.save()
+  res.redirect("/listings")
+})
 
 
 // app.get("/testListing", async (req, res) => {
