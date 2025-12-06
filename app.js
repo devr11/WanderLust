a90bfc6083d4aecea2692eaec23f5f944afdb045
Dev <rastogi.db1@gmail.com>
2025-12-06 23:20:55 +0530

    modified

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(method("_method"));
-app.engine("ejs", ejsMate)
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")))
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
