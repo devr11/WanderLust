144daba7a9c523ff0c2704939cceff4bde8c080e
Dev <rastogi.db1@gmail.com>
2025-12-06 13:54:38 +0530

    modified

--- app.js
+++ app.js
@@ -58,6 +58,8 @@ app.get("/listings/:id/edit", async (req, res)=> {
 //Update Route
 app.get("/listings/:id", async (req, res)=>{
   const {id} = req.params;
+  await Listing.findByIdAndUpdate(id, {...req.body.listing})
+  res.redirect(`/listings/${id}`)
 })
 
 // app.get("/testListing", async (req, res) => {
