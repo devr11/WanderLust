67d27b6ff5b8ae52c4b0e77427c7dd971b817843
Dev <rastogi.db1@gmail.com>
2025-12-06 14:00:31 +0530

    modified

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
-const path = require('path');
-const method = require("method-override")
+const path = require("path");
+const method = require("method-override");
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"))
-app.use(express.urlencoded({extended: true}))
-app.use(method("_method"))
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(method("_method"));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
@@ -29,38 +29,38 @@ app.get("/listings", async (req, res) => {
 });
 
 //New Route
-app.get("/listings/new", (req, res)=>{
-  res.render("listings/new")
-})
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new");
+});
 
 //Show Route
-app.get("/listings/:id", async (req, res)=>{
-  const {id} = req.params;
-  const listing = await Listing.findById(id)
-  res.render("listings/show", {listing})
-})
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show", { listing });
+});
 
 //Create Route
-app.post("/listings", async (req, res)=>{
+app.post("/listings", async (req, res) => {
   // let {title, description, image, price, country, location} = req.body;
   const newListing = new Listing(req.body.listing);
-  await newListing.save()
-  res.redirect("/listings")
-})
+  await newListing.save();
+  res.redirect("/listings");
+});
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res)=> {
-  const {id} = req.params;
-  const listing = await Listing.findById(id)
-  res.render("listings/edit", {listing})
-})
+app.get("/listings/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit", { listing });
+});
 
 //Update Route
-app.get("/listings/:id", async (req, res)=>{
-  const {id} = req.params;
-  await Listing.findByIdAndUpdate(id, {...req.body.listing})
-  res.redirect(`/listings/${id}`)
-})
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
