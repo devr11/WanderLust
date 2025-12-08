8374fca2801f6f10f3e2c61267a7e401a8065abd
Dev <rastogi.db1@gmail.com>
2025-12-08 19:25:40 +0530

    modified

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
+
 main()
   .then(() => console.log("connected to DB"))
   .catch((err) => console.log(err));
